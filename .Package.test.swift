7b65fb102401e30f1063bcb91749ff0625570ce8
Sendy Halim <sendyhalim93@gmail.com>
2017-04-08 22:56:08 +0700

    Add Swiftz to test dependency

--- .Package.test.swift
+++ .Package.test.swift
@@ -3,6 +3,7 @@ import PackageDescription
 let package = Package(
   name: "PlayingCards",
   dependencies: [
+    .Package(url: "https://github.com/typelift/Swiftz", majorVersion: 0, minor: 6),
     .Package(url: "https://github.com/Quick/Quick", majorVersion: 1, minor: 1),
     .Package(url: "https://github.com/Quick/Nimble", majorVersion: 6)
   ]
