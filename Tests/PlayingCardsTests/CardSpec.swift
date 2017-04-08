3bc5acbca13143c3acbbf1a517876dcd8a6e6601
Sendy Halim <sendyhalim93@gmail.com>
2017-04-08 22:56:49 +0700

    Add test cases for == operator

--- Tests/PlayingCardsTests/CardSpec.swift
+++ Tests/PlayingCardsTests/CardSpec.swift
@@ -1,5 +1,6 @@
 import Nimble
 import Quick
+import Swiftz
 @testable import PlayingCards
 
 class CardSpec: QuickSpec {
@@ -16,7 +17,7 @@ class CardSpec: QuickSpec {
         }
 
         context("and different suit") {
-          it("should return true") {
+          it("should return false") {
             expect(Card(value: 1, suit: .club) < Card(value: 1, suit: .diamond)) == false
             expect(Card(value: 1, suit: .heart) < Card(value: 1, suit: .club)) == false
             expect(Card(value: 1, suit: .spade) < Card(value: 1, suit: .heart)) == false
@@ -29,6 +30,35 @@ class CardSpec: QuickSpec {
           expect(Card(value: 3, suit: .club) < Card(value: 2, suit: .diamond)) == false
           expect(Card(value: 3, suit: .heart) < Card(value: 2, suit: .club)) == false
           expect(Card(value: 3, suit: .spade) < Card(value: 2, suit: .heart)) == false
+        }
+      }
+    }
+
+    describe("== operator") {
+      context("given cards with equal value") {
+        context("and same suit") {
+          it("should return true") {
+            expect(Card(value: 1, suit: .diamond)) == Card(value: 1, suit: .diamond)
+            expect(Card(value: 1, suit: .club)) == Card(value: 1, suit: .club)
+            expect(Card(value: 1, suit: .heart)) == Card(value: 1, suit: .heart)
+            expect(Card(value: 1, suit: .spade)) == Card(value: 1, suit: .spade)
+          }
+        }
+
+        context("and different suit") {
+          let suits: [Suit] = [.diamond, .club, .heart, .spade]
+          let tuple: (Suit) -> (Suit) -> (Suit, Suit) = curry({ ($0, $1) })
+          let test: (Suit, Suit) -> Void = { (x, y) in
+            it("should return false when comparing \(x) == \(y)") {
+
+              expect(Card(value: 1, suit: x)) != Card(value: 1, suit: y)
+            }
+          }
+
+          // Somehow the operadic operator doesn't work
+          suits.ap(suits.map(tuple))
+            .filter((!=))
+            .forEach(test)
         }
       }
     }
